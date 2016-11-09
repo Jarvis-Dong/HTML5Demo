5d59eaf052a8487538218cec96180989ee22d76e
Jarvis-Dong <827140187@qq.com>
2016-11-09 13:23:52 +0800

    point finish

--- Demo1/js/H5ComponentPoint.js
+++ Demo1/js/H5ComponentPoint.js
@@ -7,7 +7,14 @@ var H5ComponentPoint = function (name, cfg) {
 
     $.each(cfg.data,function(idx , item){
         var point = $('<div class="point point_'+idx+'">');
-        point.text(item[0]+'-'+item[1]);
+
+        var name = $('<div class="name">'+item[0]+'<div>');
+        var rate = $('<div class="per">'+(item[1]*100)+'%<div>');
+
+        name.append(rate);
+        point.append(name);
+
+
         var per = (item[1]/base*100)+'%';
         console.log(per);
 
